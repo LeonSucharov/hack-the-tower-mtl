710caf447908a91ad43ee0b361a70986d0ae531b
Leon Sucharov <leon.sucharov@playtech.com>
2016-10-15 12:01:24 +0100

    Added unit tests

--- src/test/scala/mtl/CalculatorTest.scala
+++ src/test/scala/mtl/CalculatorTest.scala
@@ -1,8 +1,35 @@
 package mtl
 
+import org.scalatest._
+
 /**
   * Created by lsucharov on 15/10/2016.
   */
-class CalculatorTest {
+class CalculatorTest extends FunSpec with Matchers {
+
+  it("displays an empty screen at start"){
+    val c: Calculator = new Calculator
+    c.screen shouldBe ""
+  }
+
+  it("displays an a number when it is added"){
+    val c: Calculator = new Calculator
+    c.press(1).screen shouldBe "1"
+  }
+
+  it("displays an two numbers when it is added"){
+    val c: Calculator = new Calculator
+    c.press(1).press(2).screen shouldBe "12"
+  }
+
+  it("adds two numbers"){
+    val c: Calculator = new Calculator
+    c.press(1).plus().press(2).screen shouldBe "1+2"
+  }
+
+  it("adds two numbers and equals"){
+    val c: Calculator = new Calculator
+    c.press(1).plus().press(2).equals().screen shouldBe "3"
+  }
 
 }
